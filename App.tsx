a50c03c1e7e2ab428c7ac24a89653d6d08a01fd2
Folareen <sakawahab03@gmail.com>
2023-06-10 10:02:36 +0100

    added inter fonts

--- App.tsx
+++ App.tsx
@@ -1,23 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
 import Navigation from './src/navigation';
-import Landing from './src/screens/auth/Landing';
+import * as SplashScreen from 'expo-splash-screen'
+
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
+
+  const [isLoaded] = useFonts({
+    '300': require('./src/assets/fonts/Inter-Light.ttf'),
+    '400': require('./src/assets/fonts/Inter-Regular.ttf'),
+    '500': require('./src/assets/fonts/Inter-Medium.ttf'),
+    '600': require('./src/assets/fonts/Inter-SemiBold.ttf'),
+    '700': require('./src/assets/fonts/Inter-Bold.ttf'),
+    '800': require('./src/assets/fonts/Inter-ExtraBold.ttf'),
+  })
+
+  const handleOnLayout = async () => {
+    if (isLoaded) {
+      await SplashScreen.hideAsync()
+    }
+  }
+
+  if (!isLoaded) return null
+
   return (
-    <PaperProvider>
-      <Navigation />
-    </PaperProvider>
+    <View style={{ flex: 1 }} onLayout={handleOnLayout}>
+      <PaperProvider>
+        <Navigation />
+      </PaperProvider>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
