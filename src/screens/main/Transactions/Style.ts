0cff3d503fd37a03f1df1010d3d616c432098de4
Folareen <sakawahab03@gmail.com>
2023-06-12 12:35:42 +0100

    done with transactions screen

--- src/screens/main/Transactions/Style.ts
+++ src/screens/main/Transactions/Style.ts
@@ -1,7 +1,8 @@
 import { StyleSheet } from "react-native";
+import { screenWidth } from "../../../utils/screenDimen";
 
 export const styles = StyleSheet.create({
-    container: {},
+    container: { backgroundColor: '#FFF6E5', flex: 1 },
     header: {
         flexDirection: 'row',
         padding: 15,
@@ -15,5 +16,44 @@ export const styles = StyleSheet.create({
         textAlign: 'center',
         fontSize: 18,
         fontFamily: '600'
+    },
+    dropdownsContainer: {
+        flexDirection: 'row',
+        marginVertical: 25,
+        marginHorizontal: 15,
+        columnGap: 10
+    },
+    dropdown: {
+        borderColor: '#02021A',
+        borderWidth: 1,
+        borderStyle: 'solid',
+        padding: 10,
+        borderRadius: 40,
+        backgroundColor: 'transparent',
+    },
+    monthDropdown: {
+        width: screenWidth / 3
+    },
+    categoriesDropdown: {
+        width: screenWidth / 4
+    },
+    dropdownText: {
+        fontFamily: '600', 
+        fontSize: 15,
+        textTransform: 'capitalize'
+    },
+    rowText: {
+        fontFamily: '500', 
+        fontSize: 14,
+        textTransform: 'capitalize'
+    },
+    selectedRow: {
+      backgroundColor: 'rgba(0,0,0,0.1)'   
+    },
+    selectedRowText: {
+        fontFamily: '600' 
+    },
+    transactions: {
+        
     }
 })
--- src/screens/main/Transactions/index.tsx
+++ src/screens/main/Transactions/index.tsx
@@ -1,18 +1,122 @@
-import { Text, View } from 'react-native'
+import { Text, TouchableOpacity, View, FlatList } from 'react-native'
 import React from 'react'
 import { styles } from './Style'
 import { AntDesign } from '@expo/vector-icons'
+import SelectDropdown from 'react-native-select-dropdown'
+import months from '../../../utils/months'
+import categories from '../../../utils/categories'
+import TransactionCard from '../../../components/TransactionCard'
+import { StatusBar } from 'expo-status-bar'
 
-const Transactions = () => {
+
+const transactions = [
+    {
+        category: 'shopping', description: 'buy some grocery', amount: 4000, transactionType: 'expenses', date: '10:00AM july,2020'
+    },
+    {
+        category: 'salary', description: 'buy some grocery', amount: 4000, transactionType: 'income', date: '10:00AM july,2020'
+    },
+    {
+        category: 'shopping', description: 'buy some grocery', amount: 4000, transactionType: 'expenses', date: '10:00AM july,2020'
+    },
+    {
+        category: 'salary', description: 'buy some grocery', amount: 4000, transactionType: 'income', date: '10:00AM july,2020'
+    },
+    {
+        category: 'shopping', description: 'buy some grocery', amount: 4000, transactionType: 'expenses', date: '10:00AM july,2020'
+    },
+    {
+        category: 'salary', description: 'buy some grocery', amount: 4000, transactionType: 'income', date: '10:00AM july,2020'
+    },
+    {
+        category: 'shopping', description: 'buy some grocery', amount: 4000, transactionType: 'expenses', date: '10:00AM july,2020'
+    },
+    {
+        category: 'salary', description: 'buy some grocery', amount: 4000, transactionType: 'income', date: '10:00AM july,2020'
+    },
+    {
+        category: 'shopping', description: 'buy some grocery', amount: 4000, transactionType: 'expenses', date: '10:00AM july,2020'
+    },
+]
+
+const Transactions = ({ navigation }: { navigation: any }) => {
     return (
-        <View>
+        <View style={styles.container}>
+            <StatusBar style='dark' />
 
             <View style={styles.header}>
-                <AntDesign name="arrowleft" size={24} color="black" />
+                <TouchableOpacity onPress={() => {
+                    navigation.goBack()
+                }}>
+                    <AntDesign name="arrowleft" size={24} color="black" />
+                </TouchableOpacity>
                 <Text style={styles.title}>
                     Transactions
                 </Text>
             </View>
+
+            <View style={styles.dropdownsContainer}>
+                <SelectDropdown
+                    data={months}
+                    onSelect={(selectedItem, index) => {
+                        console.log(selectedItem, index)
+                    }}
+                    buttonTextAfterSelection={(selectedItem) => {
+                        return selectedItem
+                    }}
+                    rowTextForSelection={(item) => {
+                        return item
+                    }}
+                    defaultButtonText='Month'
+                    buttonStyle={[styles.dropdown, styles.monthDropdown]}
+                    buttonTextStyle={styles.dropdownText}
+                    renderDropdownIcon={() => (
+                        <AntDesign name="down" size={18} color="black" />
+                    )}
+                    rowTextStyle={styles.rowText}
+                    selectedRowStyle={styles.selectedRow}
+                    selectedRowTextStyle={styles.selectedRowText}
+                    dropdownOverlayColor='rgba(0,0,0,0.8)'
+                />
+                <SelectDropdown
+                    data={categories}
+                    onSelect={(selectedItem, index) => {
+                        console.log(selectedItem, index)
+                    }}
+                    buttonTextAfterSelection={(selectedItem) => {
+                        return selectedItem
+                    }}
+                    rowTextForSelection={(item) => {
+                        return item
+                    }}
+                    defaultValue='all'
+                    defaultButtonText='All'
+                    buttonStyle={[styles.dropdown, styles.categoriesDropdown]}
+                    buttonTextStyle={styles.dropdownText}
+                    renderDropdownIcon={() => (
+                        <AntDesign name="down" size={18} color="black" />
+                    )}
+                    rowTextStyle={styles.rowText}
+                    selectedRowStyle={styles.selectedRow}
+                    selectedRowTextStyle={styles.selectedRowText}
+                    dropdownOverlayColor='rgba(0,0,0,0.8)'
+                />
+            </View>
+
+            <FlatList
+                style={styles.transactions}
+                data={transactions}
+                renderItem={({ item: { category, description, amount, transactionType, date } }) => (
+                    <TransactionCard amount={amount} transactionType={transactionType} category={category} date={date} description={description} />
+                )}
+                ListEmptyComponent={() => (
+                    <Text>
+                        empty
+                    </Text>
+                )}
+
+            />
+
 
         </View>
     )
