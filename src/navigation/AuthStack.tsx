d1c2b4e6adc3fd198b0ffd9b133bd988c97928a4
Folareen <sakawahab03@gmail.com>
2023-06-19 02:11:48 +0100

    added forgot password and reset password screens

--- src/navigation/AuthStack.tsx
+++ src/navigation/AuthStack.tsx
@@ -4,6 +4,8 @@ import { createStackNavigator } from '@react-navigation/stack'
 import Login from '../screens/auth/Login'
 import Signup from '../screens/auth/Signup'
 import Landing from '../screens/auth/Landing'
+import ForgotPassword from '../screens/auth/ForgotPassword'
+import ResetPassword from '../screens/auth/ResetPassword'
 
 const Stack = createStackNavigator()
 
@@ -13,6 +15,8 @@ const AuthStack = () => {
             <Stack.Screen component={Landing} name='landing' options={{ headerShown: false }} />
             <Stack.Screen component={Login} name='login' options={{ headerShown: false }} />
             <Stack.Screen component={Signup} name='signup' options={{ headerShown: false }} />
+            <Stack.Screen component={ForgotPassword} name='forgot-password' options={{ headerShown: false }} />
+            <Stack.Screen component={ResetPassword} name='reset-password' options={{ headerShown: false }} />
         </Stack.Navigator>
     )
 }
