d11192ff472c264969594b68bfdf37cf92ee3539
Folareen <sakawahab03@gmail.com>
2023-06-19 02:10:31 +0100

    created login service

---
+++ src/services/auth.ts
@@ -0,0 +1,15 @@
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import jwtDecode from "jwt-decode"
+import api from "../api"
+
+export const login = async (data: {emailAddress: string, password: string}, dispatch: (user: any) => void) => {
+    try {
+        const response = await api.post('login', data)
+        const user = await jwtDecode(response.data.token)
+        await AsyncStorage.removeItem('token')
+        await AsyncStorage.setItem('token', response.data.token)
+        dispatch(user)
+    } catch (error: any) {
+        throw new Error(error?.message || error)
+    }
+}
