16a903f893a6d1a5c41f10094b09debeeef91f71
Folareen <sakawahab03@gmail.com>
2023-06-19 02:11:59 +0100

    done with login

--- src/screens/auth/Login.tsx
+++ src/screens/auth/Login.tsx
@@ -1,24 +1,59 @@
-import { KeyboardAvoidingView, ScrollView, Text, TouchableOpacity, View } from 'react-native'
+import { ActivityIndicator, Alert, KeyboardAvoidingView, ScrollView, Text, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
-import { TextInput, Text as RNPText, Button } from 'react-native-paper'
+import { TextInput, Text as RNPText, Button, Portal, Modal } from 'react-native-paper'
 import styles from './Style'
 import { StatusBar } from 'expo-status-bar'
+import useAuthStore from '../../stores/useAuthStore'
+import { login } from '../../services/auth'
 
 const Login = ({ navigation }: { navigation: any }) => {
     const [emailAddress, setEmailAddress] = useState<string>('')
     const [password, setPassword] = useState<string>('')
+    const [submitting, setSubmitting] = useState(false)
+    const [error, setError] = useState('')
+
+    const store = useAuthStore()
+
+    const handleSubmit = async () => {
+        try {
+            setError('')
+            setSubmitting(true)
+            await login({ emailAddress, password }, store.authenticate)
+        } catch (error: any) {
+            setError(error.message)
+            console.log(error.message, 'log error')
+        } finally {
+            setSubmitting(false)
+        }
+    }
 
 
     return (
         <View style={styles.outerContainer}>
+
+            <Portal >
+                <Modal visible={submitting} dismissable={false} style={styles.overlayContainer} contentContainerStyle={styles.overlayInner}>
+                    <ActivityIndicator size={'large'} color='#7F3DFF' />
+                </Modal>
+            </Portal>
+
+            <Portal >
+                <Modal visible={error.trim().length > 0} dismissable={true} style={styles.overlayContainer} contentContainerStyle={[styles.overlayInner, styles.overlayInnerErr]} onDismiss={() => setError('')}>
+                    <Text style={styles.overlayInnerErrText}>
+                        {error}
+                    </Text>
+                </Modal>
+            </Portal>
+
             <View style={styles.container}>
                 <RNPText variant="headlineLarge" style={styles.heading}>Login</RNPText>
                 <TextInput
                     mode="outlined"
-                    label="Email"
+                    label="Email address"
                     value={emailAddress}
                     onChangeText={text => setEmailAddress(text)}
                     style={styles.input}
+                    contentStyle={styles.inputC}
                 />
                 <TextInput
                     mode="outlined"
@@ -27,6 +62,8 @@ const Login = ({ navigation }: { navigation: any }) => {
                     value={password}
                     onChangeText={text => setPassword(text)}
                     style={styles.input}
+                    secureTextEntry={true}
+                    contentStyle={styles.inputC}
                 />
 
                 <TouchableOpacity onPress={() => {
@@ -39,7 +76,7 @@ const Login = ({ navigation }: { navigation: any }) => {
                 </TouchableOpacity>
 
 
-                <Button mode='elevated' onPress={() => navigation.navigate('login')} style={styles.submitBtn} labelStyle={styles.submitBtnText} >
+                <Button mode='elevated' onPress={handleSubmit} style={styles.submitBtn} labelStyle={styles.submitBtnText} >
                     Login
                 </Button>
 
