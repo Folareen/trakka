80418755b4b929004ac8b44c2fafd4f17f787333
Folareen <sakawahab03@gmail.com>
2023-06-19 02:11:21 +0100

    added authstate

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,15 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import MainStack from './MainStack'
 import AuthStack from './AuthStack'
 import { NavigationContainer } from '@react-navigation/native'
+import useAuthStore from '../stores/useAuthStore'
+import styles from '../screens/auth/Style'
+import { ActivityIndicator } from 'react-native'
+import { Modal, Portal } from 'react-native-paper'
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import jwtDecode from "jwt-decode"
+
 
 const Navigation = () => {
-    const user = { name: 'folareen', token: 'lol404' }
-    // const user = { token: null }
+    const store = useAuthStore()
+    const [authenticating, setAuthenticating] = useState(true)
+
+
+    useEffect(() => {
+        (
+            async () => {
+                try {
+                    setAuthenticating(true)
+                    const token = await AsyncStorage.getItem('token') as string
+                    const user = await jwtDecode(token)
+                    store.authenticate(user)
+                } catch (error: any) {
+                    console.log(error)
+                } finally {
+                    setAuthenticating(false)
+                }
+            }
+        )()
+    }, [])
+
+
+    if (authenticating) {
+        return (<Portal >
+            <Modal visible={authenticating} dismissable={false} style={styles.overlayContainer} contentContainerStyle={styles.overlayInner}>
+                <ActivityIndicator size={'large'} color='#7F3DFF' />
+            </Modal>
+        </Portal>
+        )
+    }
+
+
+
     return (
         <NavigationContainer >
             {
-                user.token ?
+                store.user ?
                     <MainStack />
                     :
                     <AuthStack />
