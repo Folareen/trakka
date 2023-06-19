2ed6c0f6a5f2917db0a287126bc4363e4c43dec3
Folareen <sakawahab03@gmail.com>
2023-06-19 02:57:42 +0100

    created signup service

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -13,3 +13,16 @@ export const login = async (data: {emailAddress: string, password: string}, disp
         throw new Error(error?.message || error)
     }
 }
+
+
+export const signup = async (data:  { fullname : string, username : string, emailAddress : string, password : string, accountBalance : string }, dispatch: (user: any) => void) => {
+    try {
+        const response = await api.post('signup', data)
+        const user = await jwtDecode(response.data.token)
+        await AsyncStorage.removeItem('token')
+        await AsyncStorage.setItem('token', response.data.token)
+        dispatch(user)
+    } catch (error: any) {
+        throw new Error(error?.message || error)
+    }
+}
