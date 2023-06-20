20867e56ef6f8440a3d36320398b05d08403b9dd
Folareen <sakawahab03@gmail.com>
2023-06-20 12:34:42 +0100

    fixed month issue

--- src/screens/main/Transactions/index.tsx
+++ src/screens/main/Transactions/index.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { styles } from './Style'
 import { AntDesign } from '@expo/vector-icons'
 import SelectDropdown from 'react-native-select-dropdown'
-import months from '../../../utils/months'
+import { months } from '../../../utils/formatDate'
 import categories from '../../../utils/categories'
 import TransactionCard from '../../../components/TransactionCard'
 import { StatusBar } from 'expo-status-bar'
@@ -13,11 +13,11 @@ import { Skeleton } from 'moti/build/skeleton'
 
 const Transactions = ({ navigation }: { navigation: any }) => {
     const [transType, setTransType] = useState<'all' | 'expenses' | 'income'>('all')
-    const [month, setMonth] = useState<number>(new Date().getMonth() + 1)
+    const [month, setMonth] = useState<string>(months[new Date().getMonth()])
 
     const [refresh, setRefresh] = useState(false)
 
-    const transactions = useFetch(`transaction?type=${transType}&month=${month}`, [transType, refresh, month])
+    const transactions = useFetch(`transaction?type=${transType}&month=${months.indexOf(month) + 1}`, [transType, refresh, month])
 
     return (
         <View style={styles.container}>
@@ -37,8 +37,8 @@ const Transactions = ({ navigation }: { navigation: any }) => {
             <View style={styles.dropdownsContainer}>
                 <SelectDropdown
                     data={months}
-                    onSelect={(selectedItem, index) => {
-                        setMonth(index + 1)
+                    onSelect={(selectedItem) => {
+                        setMonth(selectedItem)
                     }}
                     buttonTextAfterSelection={(selectedItem) => {
                         return selectedItem
@@ -46,6 +46,7 @@ const Transactions = ({ navigation }: { navigation: any }) => {
                     rowTextForSelection={(item) => {
                         return item
                     }}
+                    defaultValue={month}
                     defaultButtonText='Month'
                     buttonStyle={[styles.dropdown, styles.monthDropdown]}
                     buttonTextStyle={styles.dropdownText}
@@ -59,7 +60,7 @@ const Transactions = ({ navigation }: { navigation: any }) => {
                 />
                 <SelectDropdown
                     data={categories}
-                    onSelect={(selectedItem, index) => {
+                    onSelect={(selectedItem) => {
                         setTransType(selectedItem)
                     }}
                     buttonTextAfterSelection={(selectedItem) => {
@@ -68,7 +69,7 @@ const Transactions = ({ navigation }: { navigation: any }) => {
                     rowTextForSelection={(item) => {
                         return item
                     }}
-                    defaultValue='all'
+                    defaultValue={transType}
                     defaultButtonText='All'
                     buttonStyle={[styles.dropdown, styles.categoriesDropdown]}
                     buttonTextStyle={styles.dropdownText}
