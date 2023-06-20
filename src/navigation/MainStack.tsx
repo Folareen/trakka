291d8faf8de190b19f718efb03ddc4b32c8ca327
Folareen <sakawahab03@gmail.com>
2023-06-20 10:21:51 +0100

    refactor

--- src/navigation/MainStack.tsx
+++ src/navigation/MainStack.tsx
@@ -9,44 +9,37 @@ import Profile from '../screens/main/Profile'
 import AddTransaction from '../screens/main/AddTransaction'
 import { AntDesign, MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons'
 
-const Stack = createStackNavigator()
 const Tab = createBottomTabNavigator()
-
-const TabsStack = () => (
-  <Tab.Navigator screenOptions={{
-    tabBarActiveTintColor: '#7F3DFF', tabBarInactiveTintColor: '#858585',
-    tabBarLabelStyle: { fontSize: 10, fontFamily: '500', textTransform: 'capitalize' },
-    tabBarStyle: { padding: 5, height: 60 }, tabBarItemStyle: { height: 50 }
-  }}>
-    <Tab.Screen name='home' component={Home} options={{
-      tabBarIcon: ({ color }) => <MaterialIcons name="home-filled" size={30} color={color} />
-      , headerShown: false
-    }} />
-    <Tab.Screen name='transactions' component={Transactions} options={{
-      tabBarIcon: ({ color }) => <MaterialCommunityIcons name="swap-horizontal-bold" size={30} color={color} />
-      , headerShown: false
-    }} />
-    <Tab.Screen name='add-transaction' component={AddTransaction} options={{
-      tabBarIcon: () => <AntDesign name="pluscircle" size={52} color="#7F3DFF" />
-      , headerShown: false, tabBarLabelStyle: { display: 'none' }, tabBarIconStyle: { marginTop: -60 },
-    }} />
-    <Tab.Screen name='statistics' component={Statistics} options={{
-      tabBarIcon: ({ color }) => <AntDesign name="piechart" size={30} color={color} />
-      , headerShown: false
-    }} />
-    <Tab.Screen name='profile' component={Profile} options={{
-      tabBarIcon: ({ color }) => <MaterialCommunityIcons name="account" size={30} color={color} />
-      , headerShown: false
-    }} />
-  </Tab.Navigator>
-)
 
 const MainStack = () => {
   return (
     <SafeAreaView style={{ paddingTop: Platform.OS == 'ios' ? 0 : StatusBar.currentHeight, flex: 1, }}>
-      <Stack.Navigator>
-        <Stack.Screen component={TabsStack} name='tabs-stack' options={{ headerShown: false }} />
-      </Stack.Navigator>
+      <Tab.Navigator screenOptions={{
+        tabBarActiveTintColor: '#7F3DFF', tabBarInactiveTintColor: '#858585',
+        tabBarLabelStyle: { fontSize: 10, fontFamily: '500', textTransform: 'capitalize' },
+        tabBarStyle: { padding: 5, height: 60 }, tabBarItemStyle: { height: 50 }
+      }}>
+        <Tab.Screen name='home' component={Home} options={{
+          tabBarIcon: ({ color }) => <MaterialIcons name="home-filled" size={30} color={color} />
+          , headerShown: false
+        }} />
+        <Tab.Screen name='transactions' component={Transactions} options={{
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="swap-horizontal-bold" size={30} color={color} />
+          , headerShown: false
+        }} />
+        <Tab.Screen name='add-transaction' component={AddTransaction} options={{
+          tabBarIcon: () => <AntDesign name="pluscircle" size={52} color="#7F3DFF" />
+          , headerShown: false, tabBarLabelStyle: { display: 'none' }, tabBarIconStyle: { marginTop: -60 },
+        }} />
+        <Tab.Screen name='statistics' component={Statistics} options={{
+          tabBarIcon: ({ color }) => <AntDesign name="piechart" size={30} color={color} />
+          , headerShown: false
+        }} />
+        <Tab.Screen name='profile' component={Profile} options={{
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="account" size={30} color={color} />
+          , headerShown: false
+        }} />
+      </Tab.Navigator>
     </SafeAreaView>
   )
 }
