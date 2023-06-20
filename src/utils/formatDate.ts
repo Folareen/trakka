6039cbab3e5f16a9c870c7ca4c28582682560540
Folareen <sakawahab03@gmail.com>
2023-06-20 09:35:58 +0100

    created date formatting func

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,61 @@
+export const getDateAndTime = (timestamp: string | Date) => {
+    const date = new Date(`${timestamp}`);
+
+    const day = date.getDate();
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+
+
+    const time = new Date(`${timestamp}`).toLocaleTimeString()
+
+    if (month && day && year && time) {
+        return `${month} ${day}, ${year}. ${time}`;
+    }
+
+    return 'N/A'
+};
+
+export const getDate = (timestamp: string | Date) => {
+    const date = new Date(`${timestamp}`);
+
+    const day = date.getDate();
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+
+    if (month && day && year)
+        return `${day} ${month} ${year}`;
+
+    return 'N/A'
+};
+
+export const getTime = () => {
+
+}
