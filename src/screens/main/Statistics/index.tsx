0ad8b4ab55638997b1459bd1b7923296c6de364d
Folareen <sakawahab03@gmail.com>
2023-06-21 09:22:15 +0100

    stats  screen

---
+++ src/screens/main/Statistics/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+import { styles } from './Style'
+
+const Statistics = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Statistics</Text>
+        </View>
+    )
+}
+
+export default Statistics
